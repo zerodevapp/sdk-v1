86d23f9285099f0283a432848cb489d3cc434891
Derek Chiang <me@derekchiang.com>
2023-01-06 19:15:56 -0500

    Use v2 bundlers for polygon/avax/mumbai

--- packages/sdk/src/zerodev/constants.ts
+++ packages/sdk/src/zerodev/constants.ts
@@ -4,17 +4,14 @@ export const BACKEND_URL =
 
 export const PAYMASTER_URL =
   process.env.REACT_APP_ZERODEV_PAYMASTER_URL ??
-  'https://paymaster-server.onrender.com'
+  'https://staging-paymaster.onrender.com'
 
 export { default as PAYMASTER_ABI } from './abi/paymaster_abi.json'
 
 export const BUNDLER_URL: { [key: string]: any } = {
-  '1': 'https://eth-bundler.onrender.com/rpc',
-  '5': 'https://goerli-bundler.onrender.com/rpc',
-  '137': 'https://polygon-bundler.onrender.com/rpc',
-  '43113': 'https://fuji-bundler.onrender.com/rpc',
-  '43114': 'https://avalanche-bundler.onrender.com/rpc',
-  '80001': process.env.REACT_APP_ZERODEV_BUNDLER_URL ?? 'https://bundler.onrender.com/rpc',
+  '137': 'https://polygon-bundler-v2.onrender.com/rpc',
+  '43114': 'https://avalanche-bundler-v2.onrender.com/rpc',
+  '80001': 'https://mumbai-bundler-v2.onrender.com/rpc',
 }
 
 export const ENTRYPOINT_ADDRESS = '0x0F46c65C17AA6b4102046935F33301f0510B163A'
