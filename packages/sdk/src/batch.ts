26de6657c225419edbc67378da6dee619b6aace7
leekt <leekt216@gmail.com>
2023-02-04 22:05:04 +0900

    removed TODO since we don't use hardcoded gas limit

--- packages/sdk/src/batch.ts
+++ packages/sdk/src/batch.ts
@@ -38,9 +38,6 @@ export function execBatch(signer: Signer, calls: Call[], options?: {
     'function multiSend(bytes memory transactions)',
   ], delegateSigner)
 
-  // TODO: hardcoding gas is bad.  we have to do this because the gas
-  // estimation is failing due to internally when it calls populateTransaction()
-  // in sendTransaction(), it estimates the call using call not delegate call
   return multiSend.multiSend(encodeMultiSend(calls), {
     gasLimit: options?.gasLimit,
     gasPrice: options?.gasPrice,
