f54f0958959897266ce75b64e08daecedb03c663
leekt <leekt216@gmail.com>
2023-04-04 04:28:36 +0900

    added kernel account api

--- packages/sdk/src/KernelAccountAPI.ts
+++ packages/sdk/src/KernelAccountAPI.ts
@@ -1,4 +1,149 @@
-import {
-  Kernel,
-  Kernel__factory,
-} from '@zerodevapp/contracts-new'
+import { BigNumber, BigNumberish, Contract } from 'ethers'
+
+import { arrayify, hexConcat } from 'ethers/lib/utils'
+import { Signer } from '@ethersproject/abstract-signer'
+import { BaseApiParams, BaseAccountAPI } from './BaseAccountAPI'
+import { encodeMultiSend, getMultiSendAddress } from './multisend'
+import { Call } from './execBatch'
+import { Kernel, KernelFactory, KernelFactory__factory, Kernel__factory } from "@zerodevapp/contracts-new";
+
+/**
+ * constructor params, added on top of base params:
+ * @param owner the signer object for the account owner
+ * @param index nonce value used when creating multiple accounts for the same owner
+ * @param factoryAddress address of factory to deploy new contracts (not needed if account already deployed)
+ */
+export interface KernelAccountApiParams extends BaseApiParams {
+  owner: Signer
+  index?: number
+  factoryAddress?: string
+  templateAddress?: string
+}
+
+/**
+ * An implementation of the BaseAccountAPI using Gnosis Safe.
+ * - Pass "owner" address and "index" nonce to the factory
+ * - owner signs requests using normal "Ethereum Signed Message" (ether's signer.signMessage())
+ * - nonce is a public variable "nonce"
+ * - execute method is "execTransactionFromModule()", since the entrypoint is set as a module
+ */
+export class KernelAccountAPI extends BaseAccountAPI {
+  factoryAddress?: string
+  owner: Signer
+  index: number
+
+  accountContract?: Kernel
+  factory?: KernelFactory
+
+  constructor(params: KernelAccountApiParams) {
+    super(params)
+    this.factoryAddress = params.factoryAddress
+    this.owner = params.owner
+    this.index = params.index ?? 0
+  }
+
+  async _getAccountContract(): Promise<Kernel> {
+    if (this.accountContract == null) {
+      this.accountContract = Kernel__factory.connect(await this.getAccountAddress(), this.provider)
+    }
+    return this.accountContract
+  }
+
+  /**
+   * return the value to put into the "initCode" field, if the account is not yet deployed.
+   * this value holds the "factory" address, followed by this account's information
+   */
+  async getAccountInitCode(): Promise<string> {
+    return hexConcat([
+      await this.getFactoryAddress(),
+      await this.getFactoryAccountInitCode(),
+    ])
+  }
+
+  async getFactoryAddress(): Promise<string> {
+    if (this.factoryAddress != null) {
+      return this.factoryAddress
+    }
+    throw new Error('no factory address')
+  }
+
+  async getFactoryAccountInitCode(): Promise<string> {
+    const ownerAddress = await this.owner.getAddress()
+    if (this.factory == null) {
+      if (this.factoryAddress != null && this.factoryAddress !== '') {
+        this.factory = KernelFactory__factory.connect(this.factoryAddress, this.provider)
+      } else {
+        throw new Error('no factory to get initCode')
+      }
+    }
+    return this.factory.interface.encodeFunctionData('createAccount', [ownerAddress, this.index])
+  }
+
+  async getNonce(): Promise<BigNumber> {
+    if (await this.checkAccountPhantom()) {
+      return BigNumber.from(0)
+    }
+    const accountContract = await this._getAccountContract()
+    return await accountContract["getNonce()"]()
+  }
+
+  /**
+   * encode a method call from entryPoint to our contract
+   * @param target
+   * @param value
+   * @param data
+   */
+  async encodeExecute(target: string, value: BigNumberish, data: string): Promise<string> {
+    const accountContract = await this._getAccountContract()
+
+    // the executeAndRevert method is defined on the manager
+    const managerContract = Kernel__factory.connect(accountContract.address, accountContract.provider)
+    return managerContract.interface.encodeFunctionData(
+      'executeAndRevert',
+      [
+        target,
+        value,
+        data,
+        0,
+      ])
+  }
+
+  /**
+   * encode a method call from entryPoint to our contract
+   * @param target
+   * @param value
+   * @param data
+   */
+  async encodeExecuteDelegate(target: string, value: BigNumberish, data: string): Promise<string> {
+    const accountContract = await this._getAccountContract()
+
+    // the executeAndRevert method is defined on the manager
+    const managerContract = Kernel__factory.connect(accountContract.address, accountContract.provider)
+    return managerContract.interface.encodeFunctionData(
+      'executeAndRevert',
+      [
+        target,
+        value,
+        data,
+        1,
+      ])
+  }
+
+  async encodeExecuteBatch(
+    calls: Array<Call>,
+  ): Promise<string> {
+    const multiSend = new Contract(getMultiSendAddress(), [
+      'function multiSend(bytes memory transactions)',
+    ])
+
+    const multiSendCalldata = multiSend.interface.encodeFunctionData(
+      'multiSend',
+      [encodeMultiSend(calls)]
+    )
+    return this.encodeExecuteDelegate(multiSend.address, 0, multiSendCalldata)
+  }
+
+  async signUserOpHash(userOpHash: string): Promise<string> {
+    return await this.owner.signMessage(arrayify(userOpHash))
+  }
+}
---
+++ packages/sdk/test/z-ZerodevKernelAccountAPI.test.ts
@@ -0,0 +1,176 @@
+import {
+  EntryPoint,
+  EntryPoint__factory,
+  GnosisSafeProxyFactory__factory,
+  UserOperationStruct
+} from '@zerodevapp/contracts'
+import { Wallet } from 'ethers'
+import { parseEther } from 'ethers/lib/utils'
+import { expect } from 'chai'
+import { anyValue } from '@nomicfoundation/hardhat-chai-matchers/withArgs'
+import { ethers } from 'hardhat'
+import { SampleRecipient, SampleRecipient__factory } from '@account-abstraction/utils/dist/src/types'
+import { rethrowError } from '@account-abstraction/utils'
+import { KernelAccountAPI } from '../src/KernelAccountAPI'
+import { ExecuteType } from '../src/BaseAccountAPI'
+import { KernelFactory__factory } from '@zerodevapp/contracts-new'
+
+const provider = ethers.provider
+const signer = provider.getSigner()
+const PREFIX = 'zerodev'
+
+describe.only('KernelAccountAPI', () => {
+  let owner: Wallet
+  let api: KernelAccountAPI
+  let entryPoint: EntryPoint
+  let beneficiary: string
+  let recipient: SampleRecipient
+  let accountAddress: string
+  let accountDeployed = false
+
+  before('init', async () => {
+    entryPoint = await new EntryPoint__factory(signer).deploy()
+    beneficiary = await signer.getAddress()
+
+    // standard safe singleton contract (implementation)
+    const accountFactory = await new KernelFactory__factory(signer)
+      .deploy(entryPoint.address)
+
+    recipient = await new SampleRecipient__factory(signer).deploy()
+    owner = Wallet.createRandom()
+    api = new KernelAccountAPI({
+      provider,
+      owner,
+      entryPointAddress: entryPoint.address,
+      factoryAddress: accountFactory.address,
+    })
+  })
+
+  it('#getUserOpHash should match entryPoint.getUserOpHash', async function () {
+    const userOp: UserOperationStruct = {
+      sender: '0x'.padEnd(42, '1'),
+      nonce: 2,
+      initCode: '0x3333',
+      callData: '0x4444',
+      callGasLimit: 5,
+      verificationGasLimit: 6,
+      preVerificationGas: 7,
+      maxFeePerGas: 8,
+      maxPriorityFeePerGas: 9,
+      paymasterAndData: '0xaaaaaa',
+      signature: '0xbbbb'
+    }
+    const hash = await api.getUserOpHash(userOp)
+    const epHash = await entryPoint.getUserOpHash(userOp)
+    expect(hash).to.equal(epHash)
+  })
+
+  it('should deploy to counterfactual address', async () => {
+    accountAddress = await api.getAccountAddress()
+    expect(await provider.getCode(accountAddress).then(code => code.length)).to.equal(2)
+
+    await signer.sendTransaction({
+      to: accountAddress,
+      value: parseEther('0.1')
+    })
+    const op = await api.createSignedUserOp({
+      target: recipient.address,
+      data: recipient.interface.encodeFunctionData('something', ['hello'])
+    })
+
+    await expect(entryPoint.handleOps([op], beneficiary)).to.emit(recipient, 'Sender')
+      .withArgs(anyValue, accountAddress, 'hello')
+    expect(await provider.getCode(accountAddress).then(code => code.length)).to.greaterThan(0)
+    accountDeployed = true
+  })
+
+  context('#rethrowError', () => {
+    let userOp: UserOperationStruct
+    before(async () => {
+      userOp = await api.createUnsignedUserOp({
+        target: ethers.constants.AddressZero,
+        data: '0x'
+      })
+      // expect FailedOp "invalid signature length"
+      userOp.signature = '0x11'
+    })
+    it('should parse FailedOp error', async () => {
+      const err = await entryPoint.handleOps([userOp], beneficiary)
+          .catch(error=> {
+            const errorData = error.message.split('(return data: ')[1].split(')')[0];
+            console.log(errorData)
+            const str = ethers.utils.toUtf8String('0x' + errorData.substring(202,202+44));
+            return str;
+          })
+      console.log(err);
+      expect(err).to.be.equal('AA23 reverted (or OOG)')
+    })
+    it('should parse Error(message) error', async () => {
+      await expect(
+        entryPoint.addStake(0)
+      ).to.revertedWith('must specify unstake delay')
+    })
+    it('should parse revert with no description', async () => {
+      // use wrong signature for contract..
+      const wrongContract = entryPoint.attach(recipient.address)
+      await expect(
+        wrongContract.addStake(0)
+      ).to.revertedWithoutReason()
+    })
+  })
+
+  it('should use account API after creation without a factory', async function () {
+    if (!accountDeployed) {
+      this.skip()
+    }
+    const api1 = new KernelAccountAPI({
+      provider,
+      entryPointAddress: entryPoint.address,
+      accountAddress,
+      owner
+    })
+    const op1 = await api1.createSignedUserOp({
+      target: recipient.address,
+      data: recipient.interface.encodeFunctionData('something', ['world'])
+    })
+    await expect(entryPoint.handleOps([op1], beneficiary)).to.emit(recipient, 'Sender')
+      .withArgs(anyValue, accountAddress, 'world')
+  })
+
+  it('should delegate call', async function () {
+    if (!accountDeployed) {
+      this.skip()
+    }
+    const api1 = new KernelAccountAPI({
+      provider,
+      entryPointAddress: entryPoint.address,
+      accountAddress,
+      owner
+    })
+
+    const op1 = await api1.createSignedUserOp({
+      target: recipient.address,
+      data: recipient.interface.encodeFunctionData('something', ['world'])
+    }, ExecuteType.EXECUTE_DELEGATE)
+
+    // in a delegate call, the we should find the event emitted by the account itself
+    const tx = await entryPoint.handleOps([op1], beneficiary)
+    const receipt = await tx.wait()
+    const events = receipt.events!.filter(
+      (e) => e.address === accountAddress,
+    )
+    let decodedEvent: any
+    for (const event of events) {
+      try {
+        decodedEvent = recipient.interface.decodeEventLog(
+          'Sender',
+          event.data,
+          event.topics,
+        )
+      } catch (e) {
+      }
+    }
+
+    expect(decodedEvent!.message).to.equal('world')
+  })
+})
