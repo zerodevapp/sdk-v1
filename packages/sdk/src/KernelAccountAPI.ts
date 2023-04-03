4f2771d9b79fec4abb86d9e818da6cf86d541963
Derek Chiang <me@derekchiang.com>
2023-04-03 16:53:23 -0700

    Fix compilation error

--- packages/sdk/src/KernelAccountAPI.ts
+++ packages/sdk/src/KernelAccountAPI.ts
@@ -84,7 +84,7 @@ export class KernelAccountAPI extends BaseAccountAPI {
       return BigNumber.from(0)
     }
     const accountContract = await this._getAccountContract()
-    return await accountContract["getNonce()"]()
+    return await accountContract.getNonce()
   }
 
   /**
