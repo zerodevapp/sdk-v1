489cbeb2f18bc4bfc26f7d99cc1bf6a9ece1958c
Zobeir Hamid <zobeirhamid@berkeley.edu>
2023-05-06 11:15:47 -0700

    fix: refactor to make critical path faster

--- packages/sdk/src/paymasters/index.ts
+++ packages/sdk/src/paymasters/index.ts
@@ -7,20 +7,22 @@ import { VerifyingPaymasterAPI } from "./VerifyingPaymasterAPI";
 
 export async function getPaymaster (projectId: string, paymasterUrl: string, chainId: number, entryPointAddress: string, gasToken?: SupportedGasToken): Promise<PaymasterAPI> {
   let gasTokenAddress
-  const paymasterAddress = await api.getPaymasterAddress(chainId, entryPointAddress)
   if (gasToken === 'TEST_ERC20') gasTokenAddress = '0x3870419Ba2BBf0127060bCB37f69A1b1C090992B'
   if (gasToken === 'USDC') {
     gasTokenAddress = constants.USDC_ADDRESS[chainId]
   }
-  if (gasTokenAddress !== undefined && paymasterAddress !== undefined) {
-    return new TokenPaymasterAPI(
-      projectId,
-      paymasterUrl,
-      chainId,
-      entryPointAddress,
-      gasTokenAddress,
-      paymasterAddress
-    )
+  if (gasTokenAddress !== undefined) {
+    const paymasterAddress = await api.getPaymasterAddress(chainId, entryPointAddress)
+    if (paymasterAddress !== undefined) {
+      return new TokenPaymasterAPI(
+        projectId,
+        paymasterUrl,
+        chainId,
+        entryPointAddress,
+        gasTokenAddress,
+        paymasterAddress
+      )
+    }
   }
   return new VerifyingPaymasterAPI(
     projectId,
